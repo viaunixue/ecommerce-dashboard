eaf6ab3054cab8c0f18b869b2839ca1c5c029b8c
JongHyunJung <jjh19960115@gmail.com>
2024-01-25 20:02:57 +0900

    feat: add colors entity (admin)

--- ecommerce-admin/components/main-nav.tsx
+++ ecommerce-admin/components/main-nav.tsx
@@ -31,6 +31,11 @@ export function MainNav({
             href: `/${params.storeId}/sizes`,
             label: 'Sizes',
             active: pathname === `/${params.storeId}/sizes`,
+        },
+        {
+            href: `/${params.storeId}/colors`,
+            label: 'Colors',
+            active: pathname === `/${params.storeId}/colors`,
         },    
         {
             href: `/${params.storeId}/settings`,
